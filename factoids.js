967fb010720e73c99b1dc0109beff837c17e2224
ajbiffl219 <ajbiffl219@gmail.com>
2021-01-21 16:07:34 -0700

    reconfiguring

---
+++ factoids.js
@@ -0,0 +1,11 @@
+var factoids = {
+
+    'test' : ['testing', 'this is a test', 'hellooo'],
+
+    'also test' : ['testing', 'also']
+
+};
+module.exports = {factoids}; 
+
+
+const factoids = require('./physics_bot/factoids.js').factoids;
